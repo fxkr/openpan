e7edf77b1d459cd53b1d53369df91d5fd65ed7bd
Felix Kaiser <felix.kaiser@fxkr.net>
2018-06-03 16:20:21 -0700

    Add .clang-format and format code according to Google style

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include <mbed.h>
 
 int main() {
+  // put your setup code here, to run once:
 
-    // put your setup code here, to run once:
-
-    while(1) {
-        // put your main code here, to run repeatedly:
-    }
+  while (1) {
+    // put your main code here, to run repeatedly:
+  }
 }
