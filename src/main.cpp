3eede7ab71b3568f50b417dcb113ed93fde1739f
Felix Kaiser <felix.kaiser@fxkr.net>
2018-07-21 20:55:13 -0700

    Move component allocations out of main()
    This is to the reduce the stack size in preparation for integrating
    with mbed/RTOS. In keeping with the "no global variables" spirit,
    the new global variables are all static and not used outside of main.

--- src/main.cpp
+++ src/main.cpp
@@ -15,64 +15,62 @@
 #include "hw/perf_timer.h"
 #include "hw/volatile_buffer.h"
 
+// Constants
 static const uint32_t fb_addr = LCD_FB_START_ADDRESS;
 static const uint32_t fb_size = sizeof(uint32_t) * 272 * 480;
 
-// Singletons called by interrupt handlers
+// References for use by interrupt handlers.
 static app::Application *volatile global_app = nullptr;
 static app::debug::Debug *volatile global_dbg = nullptr;
+
+// Allocate components statically due to stack size limit. Only used by main.
+static Serial serial(USBTX, USBRX);
+static app::debug::Debug dbg(serial);
+static DigitalOut led(LED1);
+static DigitalIn button(USER_BUTTON);
+static volatile app::structs::Complex<int16_t> audio_buffer_alloc[2 * 512];
+static app::debug::Counter ltdc_underrun_counter(dbg, "ltdc_underrun");
+static app::debug::Counter missed_audio_counter(dbg, "missed_audio");
+static app::debug::Counter late_audio_read_counter(dbg, "late_audio_read");
+static app::hw::PerfTimer perf_timer;
+static app::hw::CopyDMA copy_dma;
+static app::hw::ZeroDMA zero_dma;
+static app::hw::VolatileBuffer<uint8_t> buf0(
+    dbg, zero_dma, fb_addr + 0 * fb_size, fb_size);
+static app::hw::VolatileBuffer<uint8_t> buf1(
+    dbg, zero_dma, fb_addr + 1 * fb_size, fb_size);
+static app::hw::VolatileBuffer<uint8_t> buf2(
+    dbg, zero_dma, fb_addr + 2 * fb_size, fb_size);
+static app::hw::VolatileBuffer<uint32_t> buf3(
+    dbg, zero_dma, fb_addr + 3 * fb_size, fb_size);
+static app::hw::VolatileBuffer<uint32_t> buf4(
+    dbg, zero_dma, fb_addr + 4 * fb_size, fb_size);
+static app::hw::VolatileBuffer<uint32_t> buf5(
+    dbg, zero_dma, fb_addr + 5 * fb_size, fb_size);
+static app::hw::VolatileBuffer<uint8_t> wf_buf(
+    dbg, zero_dma, fb_addr + 6 * fb_size, fb_size);
+static app::hw::VolatileBuffer<app::structs::Complex<int16_t>> audio_buf(
+    dbg, zero_dma, (uint32_t)&audio_buffer_alloc, sizeof(audio_buffer_alloc));
+static app::hw::VolatileTripleBuffer<uint8_t> layer0(dbg, buf0, buf1, buf2);
+static app::hw::VolatileTripleBuffer<uint32_t> layer1(dbg, buf3, buf4, buf5);
+static app::ui::Waterfall waterfall(wf_buf, copy_dma, 480, 272);
+static app::hw::Display display(
+    dbg, layer0, layer1, copy_dma, ltdc_underrun_counter);
+static app::hw::Recorder recorder(
+    dbg, audio_buf, missed_audio_counter, late_audio_read_counter);
+static app::ui::Canvas canvas(480, 272);
+static app::Application application(dbg, display, canvas, recorder, waterfall);
 
 int main() {
   HAL_Init();
 
-  Serial serial(USBTX, USBRX);
-
-  app::debug::Debug dbg(serial);
   app::debug::init(dbg);
   global_dbg = &dbg;
 
   dbg.printf("\nInit...\n");
   dbg.printf("Speed: %d Hz.\n", SystemCoreClock);
 
-  BSP_SDRAM_Init();
-
-  DigitalOut led(LED1);
-  DigitalIn button(USER_BUTTON);
-
-  volatile app::structs::Complex<int16_t> audio_buffer_alloc[2 * 512];
-
-  app::debug::Counter ltdc_underrun_counter(dbg, "ltdc_underrun");
-  app::debug::Counter missed_audio_counter(dbg, "missed_audio");
-  app::debug::Counter late_audio_read_counter(dbg, "late_audio_read");
-  app::hw::PerfTimer perf_timer;
-  app::hw::CopyDMA copy_dma;
-  app::hw::ZeroDMA zero_dma;
-  app::hw::VolatileBuffer<uint8_t> buf0(
-      dbg, zero_dma, fb_addr + 0 * fb_size, fb_size);
-  app::hw::VolatileBuffer<uint8_t> buf1(
-      dbg, zero_dma, fb_addr + 1 * fb_size, fb_size);
-  app::hw::VolatileBuffer<uint8_t> buf2(
-      dbg, zero_dma, fb_addr + 2 * fb_size, fb_size);
-  app::hw::VolatileBuffer<uint32_t> buf3(
-      dbg, zero_dma, fb_addr + 3 * fb_size, fb_size);
-  app::hw::VolatileBuffer<uint32_t> buf4(
-      dbg, zero_dma, fb_addr + 4 * fb_size, fb_size);
-  app::hw::VolatileBuffer<uint32_t> buf5(
-      dbg, zero_dma, fb_addr + 5 * fb_size, fb_size);
-  app::hw::VolatileBuffer<uint8_t> wf_buf(
-      dbg, zero_dma, fb_addr + 6 * fb_size, fb_size);
-  app::hw::VolatileBuffer<app::structs::Complex<int16_t>> audio_buf(
-      dbg, zero_dma, (uint32_t)&audio_buffer_alloc, sizeof(audio_buffer_alloc));
-  app::hw::VolatileTripleBuffer<uint8_t> layer0(dbg, buf0, buf1, buf2);
-  app::hw::VolatileTripleBuffer<uint32_t> layer1(dbg, buf3, buf4, buf5);
-  app::ui::Waterfall waterfall(wf_buf, copy_dma, 480, 272);
-  app::hw::Display display(
-      dbg, layer0, layer1, copy_dma, ltdc_underrun_counter);
-  app::hw::Recorder recorder(
-      dbg, audio_buf, missed_audio_counter, late_audio_read_counter);
-  app::ui::Canvas canvas(480, 272);
-  app::Application application(dbg, display, canvas, recorder, waterfall);
-
+  crash_if(dbg, SDRAM_OK != BSP_SDRAM_Init());
   crash_if(dbg, 0 != copy_dma.Init());
   crash_if(dbg, 0 != zero_dma.Init());
   crash_if(dbg, 0 != buf0.Init());
